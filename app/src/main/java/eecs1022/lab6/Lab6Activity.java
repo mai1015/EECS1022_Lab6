be6c7e52c50c2bff9395707ccb4b1d49c9134099
mai1015 <i@mai1015.com>
2018-04-19 14:59:35 -0400

    Initial commit

---
+++ app/src/main/java/eecs1022/lab6/Lab6Activity.java
@@ -0,0 +1,15 @@
+package eecs1022.lab6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab6Activity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab6);
+    }
+}
